a3b9fcf50af9f87aac4f8c40578eeb927a0d507b
szy39 <138227093+szy39@users.noreply.github.com>
2025-10-06 15:06:54 +0300

    first commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
+    <div>
    <h1 className='text-white '>Hi, I'm Eymen</h1>
+   <p>Lorem ipsum dolor sit amet.</p>
+   <button className='bg-blue-500 text-white px-4 py-2 rounded-md'>Contact Me</button>
+   </div>
   )
 }
 
