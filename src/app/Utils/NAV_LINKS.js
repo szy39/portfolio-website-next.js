e0c89eaf1c3e2df0c94dc969f8691aa39b383999
szy39 <138227093+szy39@users.noreply.github.com>
2025-10-08 19:32:24 +0300

    create pages via next

---
+++ src/app/Utils/NAV_LINKS.js
@@ -0,0 +1,21 @@
+const NAV_LINKS = [
+    {
+      href: "/",
+      title: "Home"
+    },
+    
+    {
+      href: "/about",
+      title: "About"
+    },
+    {
+      href: "/projects",
+      title: "Projects"
+    },
+    {
+      href: "/contact",
+      title: "Contact"
+    }
+  ]
+  
+export default NAV_LINKS
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -27,9 +27,9 @@ const HeroSection = () => {
          Javascript / Typescript / React / Next.js / Vue.js / Node.js / SCSS / CSS Frameworks / API Integration
          </p>
          <div>
-          <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white font-semibold '>Hire Me !</button>
-          <button className='px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3 font-semibold'>
-            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+          <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  text-white font-semibold '>Hire Me !</button>
+          <button className='px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  text-white  mt-3 font-semibold'>
+            <span className='block bg-[#121212]  rounded-full px-5 py-2'>Download CV</span>
             </button>
          </div>
      </div>
